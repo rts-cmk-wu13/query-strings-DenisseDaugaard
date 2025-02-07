0d0458a6050b20b9f980614772f701c06027f1d8
DenisseDaugaard <U2478723@rts-365.dk>
2025-02-07 11:22:09 +0100

    opgave løst, i gang med ekstra opgave

--- scrip.js
+++ scrip.js
@@ -15,10 +15,46 @@ then(data => {
                                 <img src="img/${destination.image}" alt="Apartament image">
                                 </figure>
                                 <section class="apartament__card__text">
-                                <i class="fa-solid fa-heart"></i><a href="destination.html?id=${destination.id}"> MORE</a>
+                                <i class="fa-solid fa-heart favorite" data-id="${destination.id}"></i><a href="destination.html?id=${destination.id}"> MORE</a>
                                 </section>
                             </div>
                             `).join("")}</section>`
+                            
 
     bodyElm.append(apartaments)
+
+    markAsFavorite ()
 })
+
+
+
+/* -------------------choose a favorit ekstra opgave -------------------- */
+let addedFavorites = JSON.parse(localStorage.getItem("favorites")) || []
+//console.log(addedFavorites);
+
+
+
+
+function markAsFavorite (){
+    let favorites = document.querySelectorAll(".favorite")
+    console.log(favorites);
+
+    favorites.forEach( favorite => {
+        favorite.addEventListener("click", event => {
+            if (event.target){
+                favorite.style.color ="red"
+                let favoriteId = favorite.dataset.id
+                localStorage.setItem("favorites", JSON.stringify(favoriteId))
+
+                if(localStorage.getItem("favorites")){
+                    console.log("hello");
+                    console.log(JSON.parse(localStorage.getItem("favorites")));
+                    console.log(favorite);
+                    
+                }
+            }
+        })
+    })
+
+
+}
