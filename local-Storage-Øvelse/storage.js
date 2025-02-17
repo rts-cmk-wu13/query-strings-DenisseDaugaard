3f1ad1576afb929e0cda730130225ab84ddbb8f7
DenisseDaugaard <U2478723@rts-365.dk>
2025-02-17 10:17:41 +0100

    opg 6-8

--- local-Storage-Øvelse/storage.js
+++ local-Storage-Øvelse/storage.js
@@ -11,3 +11,16 @@ console.log("Gemt brugernavn:", gemtNavn);
 let kage = localStorage.getItem("cheeseCake")
 console.log("Gemt kage:", kage);
 
+// konvertere data I
+
+let farver = ["rød" ,"grøn", "blå"]
+localStorage.setItem("farveArray", JSON.stringify(farver))
+
+// konvertere data II
+
+let hentedeFarver = JSON.parse(localStorage.getItem("farveArray"))
+console.log(hentedeFarver);
+
+// slette data 
+localStorage.removeItem("brugerNavn")
+
