5f264ddb380eba603c338f37600e5489dac4e894
DenisseDaugaard <U2478723@rts-365.dk>
2025-02-19 12:07:48 +0100

    dark-mode starte her

--- scrip.js
+++ scrip.js
@@ -1,9 +1,27 @@
-fetch("data/destinations.json")
+let favorites = readFromLocalStorage("favoritePlaces") || []
+console.log(favorites);
+
+//if (!favorites) favorites = []
+
+
+fetch("data/destinations.json") 
 .then(response => response.json()).
 
 then(data => {
     let bodyElm = document.querySelector("body")
-    console.log(bodyElm);
+    //console.log(bodyElm);
+
+    let headerElm= document.createElement("header")
+    headerElm.innerHTML = `<nav>
+                            <div class="ckeck-box__container">
+                                <label for="">Go to dark mode</label>
+                                <label class="switch">
+                                <input type="checkbox">
+                                <span class="slider round"></span>
+                                </label>
+                            </div>
+
+                           </nav>`
     
     let apartaments = document.createElement("section")
     apartaments.classList.add("apartaments")
@@ -15,45 +33,74 @@ then(data => {
                                 <img src="img/${destination.image}" alt="Apartament image">
                                 </figure>
                                 <section class="apartament__card__text">
-                                <i class="fa-solid fa-heart favorite" data-id="${destination.id}"></i><a href="destination.html?id=${destination.id}"> MORE</a>
+                                <i class="fa-solid fa-heart favorite ${favorites.includes(destination.id.toString())? "favorited" : ""}" data-id="${destination.id}"></i><a href="destination.html?id=${destination.id}"> MORE</a>
                                 </section>
                             </div>
                             `).join("")}</section>`
                             
+    apartaments.querySelectorAll(".favorite").forEach(function(heart){
+        heart.addEventListener("click", function(event){
+            let currentId = event.target.dataset.id
 
-    bodyElm.append(apartaments)
+           if(favorites.includes(currentId)) {
+            let newFavorites = favorites.filter(id => id != currentId)
+            favorites = newFavorites
+            event.target.classList.remove("favorited")
+            console.log(favorites);
+           } else{
+            favorites.push(currentId)
+            event.target.classList.add("favorited")
+            console.log(favorites);
+           }
+
+           saveToLocalStorage("favoritePlaces", favorites)
+            
+        })
+    })
+               
+    bodyElm.append( headerElm, apartaments)
+    })
+                            
 
     
-        let favoriteIcons = document.querySelectorAll(".favorite")
-        let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
-        // 
-        favoriteIcons.forEach(icon => {
-          let destinationId = icon.getAttribute("data-id")// 
-            if (favorites.includes(destinationId)) {
-                icon.classList.add("favorited")
-            }
+    /**
+     * 
+     * @param {string} key -key to be use in local storage 
+     * @param {string / number / boolean / object / any[] } value -value to be saved
+     * @returns {string}
+     */
+    function saveToLocalStorage (key, value){
 
-            icon.addEventListener("click", () => {
-                addRedColor(destinationId, icon)
-            })
-        })
+        localStorage.setItem(key, JSON.stringify(value))
+        return "Data was saved with the key" + key
+    }
 
 
-        function addRedColor(destinationId, icon) { // here we add some css to the icons or delete from local storage with each click
-            let index = favorites.indexOf(destinationId);
+    /**
+     * 
+     * @param {string} key -key to be use from loca storage 
+     * @returns {string / number / boolean / object / any[] }
+     */
 
-            if (index === -1) { // this is what happens with every click if index is not found in the local storage !!
-                favorites.push(destinationId)
-                icon.classList.add("favorited")
-            } else {
-                favorites.splice(index, 1)
-                icon.classList.remove("favorited")
-            }
-
-            // Save the items in the local storage!
-            localStorage.setItem("favorites", JSON.stringify(favorites));
-        }
-})
+    function readFromLocalStorage (key){
+        return JSON.parse(localStorage.getItem(key))
+    }
 
 
+    function deleteFromLocalStorage (key){
+        localStorage.removeItem(key)
+       return "the element" + key + "was deleted."
+    }
+
+
+
+    let success = saveToLocalStorage()
+    console.log(success);
+    
+   let myfavorites = readFromLocalStorage("favorites")
+   console.log(myfavorites);
+
+   let deletedSuccess =  deleteFromLocalStorage("favorites")
+   console.log(deletedSuccess);
+   
